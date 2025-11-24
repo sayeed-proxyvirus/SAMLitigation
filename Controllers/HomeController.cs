e45c0161bed14e9044a3356d3199acc1e9fa1c94
sayeed-proxyvirus <aminsayeeed32@gmail.com>
2025-11-24 16:52:30 +0600

    m

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,11 +4,12 @@ using SAMLitigation.Models.ApplicationDbContext;
 using SAMLitigation.Models.AuthorizeAttribute;
 using SAMLitigation.Models.ViewModel;
 using SAMLitigation.Services;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace SAMLitigation.Controllers
 {
-    [CustomAuthorize]
+    //[CustomAuthorize]
     public class HomeController : Controller
     {  
         private readonly DashboardService dashboardService;
@@ -22,7 +23,8 @@ namespace SAMLitigation.Controllers
         {
             DashboardViewModel viewModel = new DashboardViewModel();
 
-            //viewModel = dashboardService.GetDashboardAllCount();
+            viewModel = dashboardService.GetDashboardAllCount();
+            //PrepareMenuTree();
 
             return View(viewModel);
         }
@@ -37,5 +39,58 @@ namespace SAMLitigation.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        //public void PrepareMenuTree() 
+        //{
+        //    try 
+        //    {
+        //        var items = new List<Node>
+        //        {
+        //            new Node { Code = "A", ParentCode = null },
+        //            new Node { Code = "B", ParentCode = "A" },
+        //            new Node { Code = "C", ParentCode = "A" },
+
+        //            new Node { Code = "D", ParentCode = "B" },
+        //            new Node { Code = "E", ParentCode = "B" },
+
+        //            new Node { Code = "F", ParentCode = "C" },
+
+        //            new Node { Code = "G", ParentCode = null },
+        //            new Node { Code = "H", ParentCode = "G" },
+        //        };
+
+        //        BuildTree(items);
+
+        //    }
+        //    catch 
+        //    {
+        //        throw;
+        //    }
+        //}
+
+        //public List<Node> BuildTree(List<Node> nodes)
+        //{
+        //    // Build lookup for faster access
+        //    var lookup = nodes.ToDictionary(x => x.Code);
+
+        //    List<Node> rootNodes = new List<Node>();
+
+        //    foreach (var node in nodes)
+        //    {
+        //        if (string.IsNullOrEmpty(node.ParentCode))
+        //        {
+        //            // Root node
+        //            rootNodes.Add(node);
+        //        }
+        //        else if (lookup.TryGetValue(node.ParentCode, out var parent))
+        //        {
+        //            parent.Children.Add(node); // add child to parent
+        //        }
+        //    }
+
+        //    return rootNodes;
+        //}
+
     }
 }
